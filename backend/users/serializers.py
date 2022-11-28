0e84f725287c762df27a89dbc1c9f573f53221cd
hamza <hamza@codeplumbers.eu>
2022-11-28 23:04:52 +0100

    added status to user profile model

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .serializers import *
 from .models import Profile
+from django.contrib.auth.models import User
     
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -31,3 +32,33 @@ class UserSerializer(serializers.ModelSerializer):
             'last_name',
             'email'
         )
+
+class UserCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'email',
+            'first_name',
+            'last_name',
+            'username',
+            'password'
+        )
+
+        # These fields are only editable (not displayed) and have to be a part of 'fields' tuple
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
+    
+    def create(self, validated_data):
+        """Create and return a new user."""
+
+        user = User(
+            email = validated_data['email'],
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name'],
+            username = validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
